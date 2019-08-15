ba0a5a4bbdccfdecdd5e6af77323d346f4991e1e
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-15 11:08:17 -0600

    Update versionCheck.js

--- utils/versionCheck.js
+++ utils/versionCheck.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const pkg = require("../package");
 
 module.exports = async () => {
-    const request = await fetch("https://raw.githubusercontent.com/GrimDesignsFiveM/Discord-Verification-Bot/master/package.json?token=ALJF5U3PRA3ZKVCKUYW6R6C5KWING");
+    const request = await fetch("https://raw.githubusercontent.com/GrimDesignsFiveM/Discord-Verification-Bot/master/package.json");
     const json = await request.json();
 
     return {
