8b5f3bbb07262dfcd27e2a8b6cb387db56d7014c
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-18 06:33:31 -0600

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -19,7 +19,8 @@ module.exports = async function(message) {
         const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
         const embed = new RichEmbed()
             .setTitle("Verification")
-            .setDescription("Please solve this captcha by sending `" + this.config.prefix + "verify [code]` in <#" + message.channel.id + ">")
+            .setDescription("This server is protected by Mr Clean a project that prevents servers from being raided.\n" +
+                "Please solve this captcha by sending `" + this.config.prefix + "verify [code]` in <#" + message.channel.id + ">")
             .attachFile({ attachment: buffer, name: "captcha.jpeg" })
             .setImage("attachment://captcha.jpeg");
         message.author.send(embed).catch(() => {
