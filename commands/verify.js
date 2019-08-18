0665d6fdf2a6b7e565ba97ebe7857864546b4732
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-18 07:46:01 -0600

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -26,6 +26,7 @@ module.exports = async function(message) {
         const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
         const embed = new RichEmbed()
             .setTitle("Verification")
+            .setColor(36393f)
             .setDescription("Please solve this captcha by sending `" + this.config.prefix + "verify [code]` in <#" + message.channel.id + ">")
             .attachFile({ attachment: buffer, name: "captcha.jpeg" })
             .setImage("attachment://captcha.jpeg");
